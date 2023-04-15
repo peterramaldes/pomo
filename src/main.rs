e7a98d29d142ccc2a7e380f67e52a89b296c2706
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-04-14 22:11:02 -0300

    Collecting the arguments from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
+// Pomo is a terminal based time tracking
+//
+// Features:
+// - [ ] Start timing
+// - [ ] End timing
+// - [ ] Store time elapsed time
+// - [ ] Integrate with Tmux
+
+
+// TODO: fix if don´t pass any command this program broken
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let cmd = &args[1]; 
+
+    if cmd == "start" {
+        panic!("implement start");
+    } else if cmd == "stop" {
+        panic!("implement stop");
+    }
 }
